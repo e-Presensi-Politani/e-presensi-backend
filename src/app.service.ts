5f40a59cc12de13f6886f253bbda03a63287a3e4
m-ghozi <ghozi286@gmail.com>
2025-04-10 14:58:05 +0700

    🆕 add : config module

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'e-Presensi Politani API is running ✅';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('APP_PORT');
+  if (!port) {
+    throw new Error('APP_PORT is not defined in the environment variables');
+  }
+  await app.listen(port);
 }
+
 bootstrap();
