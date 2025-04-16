36a7d8192e82a56630f0bd9d5335919474c2573f
m-ghozi <ghozi286@gmail.com>
2025-04-17 01:35:15 +0700

    🛠️ fix : check-in/out dto

--- src/attendance/dto/check-in.dto.ts
+++ src/attendance/dto/check-in.dto.ts
@@ -1,17 +1,21 @@
 // src/attendance/dto/check-in.dto.ts
 import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class CheckInDto {
   @IsNotEmpty()
   @IsNumber()
+  @Transform(({ value }) => parseFloat(value))
   latitude: number;
 
   @IsNotEmpty()
   @IsNumber()
+  @Transform(({ value }) => parseFloat(value))
   longitude: number;
 
   @IsOptional()
   @IsNumber()
+  @Transform(({ value }) => value ? parseFloat(value) : undefined)
   accuracy?: number;
 
   @IsOptional()
