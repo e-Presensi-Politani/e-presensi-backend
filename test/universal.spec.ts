ef859cf881b0e199390a25be37db2c2b4a6e2f3e
m-ghozi <ghozi286@gmail.com>
2025-05-02 20:37:44 +0700

    🧪 test : add simple test

---
+++ test/universal.spec.ts
@@ -0,0 +1,21 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from '../src/app.controller';
+import { AppService } from '../src/app.service';
+
+describe('Universal App Test', () => {
+  let appController: AppController;
+
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = moduleRef.get<AppController>(AppController);
+  });
+
+  it('should return API running message', () => {
+    const message = appController.getHello();
+    expect(message).toMatch(/e-?Presensi Politani API is running/i);
+  });
+});
