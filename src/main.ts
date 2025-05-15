346ab26e54717cb1c4b11dfeab96d299c08dd341
m-ghozi <ghozi286@gmail.com>
2025-05-15 20:35:31 +0700

    💫 update : CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,12 @@ async function bootstrap() {
   );
 
   // Enable CORS
-  app.enableCors();
+  app.enableCors({
+    origin: configService.frontendUrl || 'http://localhost:5173', // Use the client URL from config or default to localhost:5173
+    credentials: true, // Allow credentials (cookies, authorization headers)
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 
   await app.listen(configService.port);
 
